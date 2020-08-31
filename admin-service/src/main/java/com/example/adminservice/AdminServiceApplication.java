35eb39eb77d2f410902b5c5053dd9c896623faea
Bingxuan Ying <yingbingxuan@gmail.com>
2020-08-30 19:22:54 -0700

    Add discovery service

--- admin-service/src/main/java/com/example/adminservice/AdminServiceApplication.java
+++ admin-service/src/main/java/com/example/adminservice/AdminServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.adminservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AdminServiceApplication {
 
 	public static void main(String[] args) {
---
+++ discovery-service/src/main/java/com/example/discoveryservice/DiscoveryServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServiceApplication.class, args);
+	}
+
+}
